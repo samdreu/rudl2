c294cad89d4a4318251fe9e19a4ccbb9c8f172fc
Samantha Dreussi <samdreussi@me.com>
2026-02-22 19:07:04 -0800

    first draft at executor

--- copper-sim/src/executor.rs
+++ copper-sim/src/executor.rs
@@ -3,7 +3,7 @@ use std::pin::Pin;
 use std::sync::{Arc, Mutex};
 use std::task::{Context, RawWaker, RawWakerVTable, Waker};
 
-use copper_core::{Clock, ClockDomain, State};
+use copper_core::{Clock, ClockDomain};
 
 fn noop_waker() -> Waker {
     fn clone(_: *const ()) -> RawWaker { RawWaker::new(std::ptr::null(), &VTABLE) }
@@ -16,8 +16,6 @@ fn noop_waker() -> Waker {
 
 pub struct HardwareExecutor {
     tasks: Vec<Pin<Box<dyn Future<Output = ()>>>>,
-    /// Closures that advance each state
-    state_commits: Arc<Mutex<Vec<Box<dyn Fn() + Send>>>>,
     cycle: u64,
 }
 
@@ -25,65 +23,36 @@ impl HardwareExecutor {
     pub fn new() -> Self {
         Self {
             tasks: Vec::new(),
-            state_commits: Arc::new(Mutex::new(Vec::new())),
             cycle: 0,
         }
     }
-    
-    pub fn state_factory(&self) -> StateFactory {
-        StateFactory {
-            commits: Arc::clone(&self.state_commits),
-        }
+
+    pub fn spawn<F>(&mut self, future: F) 
+    where
+        F: Future<Output = ()> + 'static,
+    {
+        self.tasks.push(Box::pin(future));
     }
-    
-    pub fn spawn(&mut self, task: impl Future<Output = ()> + 'static) {
-        self.tasks.push(Box::pin(task));
-    }
-    
+
     pub fn tick_clock<Domain: ClockDomain>(&mut self, clk: &mut Clock<Domain>) {
-        // 1) Clock edge (wake tasks waiting on tick)
         clk.advance();
 
-        // 2) Commit all registered states (flops update on edge)
-        {
-            let commits = self.state_commits.lock().unwrap();
-            for commit_fn in commits.iter() {
-                commit_fn();
-            }
-        }
-
-        // 3) Poll tasks once (combinational logic based on new state)
         let waker = noop_waker();
-        let mut cx = Context::from_waker(&waker);
+        let mut context = Context::from_waker(&waker);
         for task in &mut self.tasks {
-            let _ = task.as_mut().poll(&mut cx);
-        }
+            let _ = task.as_mut().poll(&mut context);
+        } 
 
         self.cycle += 1;
     }
-    
+
     pub fn cycle(&self) -> u64 {
         self.cycle
     }
 }
 
-#[derive(Clone)]
-pub struct StateFactory {
-    commits: Arc<Mutex<Vec<Box<dyn Fn() + Send>>>>,
-}
-
-impl StateFactory {
-    pub fn create<T: Clone + Send + 'static>(&self, initial: T) -> State<T> {
-        let state = State::new(initial);
-        
-        // Create a closure that advances this specific state
-        let state_clone = state.clone();
-        let commit_fn = move || {
-            state_clone.advance_internal();
-        };
-        
-        self.commits.lock().unwrap().push(Box::new(commit_fn));
-        
-        state
+impl Default for HardwareExecutor {
+    fn default() -> Self {
+        Self::new()
     }
 }
